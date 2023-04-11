64533fca717ae24a52c06bc61b6b636de5a3ca5e
Daevah Taylaran <daevtayl@gmail.com>
2023-04-11 06:39:43 -0700

    update application model

--- models/application.js
+++ models/application.js
@@ -2,7 +2,47 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const applicationSchema = new Schema({
-  name: { type: String, required: true },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  applicationUrl: { 
+    type: String, 
+    required: true 
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  company: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  salaryExpectation: {
+    type: Number
+  },
+  status: {
+    type: String,
+    enum: [
+      'Waiting',
+      'Received Offer',
+      'Rejected',
+      'Interviewing'
+    ],
+    default: 'Waiting'
+  },
+  priority: {
+    type: Boolean,
+    default: false
+  },
+  contact: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Event'
+    }
+  ]
 }, {
   timestamps: true
 });
