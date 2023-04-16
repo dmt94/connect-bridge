7ea90fcb764218607638918d22fd15e1b6ef4215
Daevah Taylaran <daevtayl@gmail.com>
2023-04-15 22:22:31 -0700

    update task form

--- src/components/ViewApplication/ViewApplication.jsx
+++ src/components/ViewApplication/ViewApplication.jsx
@@ -6,6 +6,12 @@ const ViewApplication = ({ application }) => {
   const [toggle, setToggle] = useState(false);
   const [taskLimit, setTaskLimit] = useState(false);
   const [checkTask, setCheckTask] = useState(false);
+  const [task, setTask] = useState({
+    name: "",
+    date: "",
+    status: "In-progress",
+    description: ""
+  })
 
   function handleToggle(evt) {
     evt.preventDefault();
@@ -28,37 +34,42 @@ const ViewApplication = ({ application }) => {
   }
 
   return ( 
-    <div className="main-div">
-      <h3>{application.status}</h3>
-      <h3>{application.priority}</h3>
-      <h2>{application.role}</h2>
-      <h4>{application.type}</h4>
-      <p>{application.company}</p>
-      <p>{application.companyWebsite}</p>
-      <p>{application.industry}</p>
-      <h4>{new Date(application.date).toLocaleDateString()}</h4>
-      <p>{application.applicationUrl}</p>
-      <p>{application.environment}</p>
-      <h2>Location:</h2>
-      <p>{application.location}</p>
-      <p>{application.description}</p>
-      <p>{application.salary}</p>
-      <div>
-        <p>References</p>
-      </div>
-      <div>
-        <p>Contacts Associated</p>
-      </div>
-      <div>
+    <div className="main-div view-application-div">
+      <div className="flex-r">
+        <div className="left-div">
+          <h3>{application.status}</h3>
+          <h3>{application.priority}</h3>
+          <h2>{application.role}</h2>
+          <h4>{application.type}</h4>
+          <p>{application.company}</p>
+          <p>{application.companyWebsite}</p>
+          <p>{application.industry}</p>
+          <h4>{new Date(application.date).toLocaleDateString()}</h4>
+          <p>{application.applicationUrl}</p>
+          <p>{application.environment}</p>
+          <h2>Location:</h2>
+          <p>{application.location}</p>
+          <p>{application.description}</p>
+          <p>{application.salary}</p>
+          <div>
+            <p>References</p>
+          </div>
+          <div>
+            <p>Contacts Associated</p>
+          </div>
+          </div>
+        </div>
+
+        <div>
         <h2>Tasks</h2>
-        <ul>
-          <li>
-            <div className="flex-r task-list">
-              <span onClick={(evt) => {crossTask(evt)}}>List item</span>
-              <a href="">Delete</a>
-            </div>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <div className="flex-r task-list">
+                <span onClick={(evt) => {crossTask(evt)}}>List item</span>
+                <a href="">Delete</a>
+              </div>
+            </li>
+          </ul>
         {toggle ? (
           <form action="" className="flex-c">
             
