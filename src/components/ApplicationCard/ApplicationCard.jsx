bec1b20f3692f328e893321f6f4a34c9da4b6913
Daevah Taylaran <daevtayl@gmail.com>
2023-04-22 23:57:14 -0700

    update application card

--- src/components/ApplicationCard/ApplicationCard.jsx
+++ src/components/ApplicationCard/ApplicationCard.jsx
@@ -2,11 +2,27 @@ import { Link } from 'react-router-dom';
 import "./ApplicationCard.css";
 
 const ApplicationCard = ({application, deleteApplication, contacts}) => {
+
+  function checkStatus(status) {
+    let tag = 'tag';
+    if (status === 'Waiting') {
+      return `${tag} waiting`;
+    } else if (status === 'Received Offer') {
+      return `${tag} received-offer`;
+    } else if (status === 'Rejected') {
+      return `${tag} rejected`;
+    } else {
+      return `${tag} rejected`;
+    }
+  }
+
   return ( 
     <div className="application-card">
         <h3>{application.priority ? "⭐ Priority " : ""}</h3>
       <div className="flex-r app-card-top-row">
-        <h2 className=''>{application.status}</h2>
+        <h2 className={
+          checkStatus(application.status)
+        }>{application.status}</h2>
         <button className='wide-delete-btn' onClick={() => {deleteApplication(application._id)}}>Delete</button>
       </div>
       <h1>{application.role}</h1>
